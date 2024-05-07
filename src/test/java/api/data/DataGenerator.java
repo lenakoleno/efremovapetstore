ea72ca22fce2c7a4ae6eab5b3e5f461d51957f24
sofiyaefremova <sefremova@rvision.ru>
2024-05-07 13:40:41 +0300

    Initial commit

--- src/test/java/api/data/DataGenerator.java
+++ src/test/java/api/data/DataGenerator.java
@@ -1,40 +1,9 @@
 package api.data;
 
-import api.models.Order;
 import api.models.Pet;
-import api.models.User;
 import com.github.javafaker.Faker;
 
 public class DataGenerator {
-    public static User getUser(int minimumLength, int maximumLength, boolean includeUppercase,
-                                  boolean includeSpecial, boolean includeDigit) {
-        Faker faker = new Faker();
-        return User.builder()
-                .id(faker.hashCode())
-                .userName(faker.name().username())
-                .firstName(faker.name().username())
-                .lastName(faker.name().username())
-                .email(faker.internet().emailAddress())
-                .password(faker.internet().password(minimumLength, maximumLength, includeUppercase,
-                        includeSpecial, includeDigit))
-                .phone(faker.phoneNumber().phoneNumber())
-                .userStatus(faker.hashCode())
-                .build();
-    }
-
-    public static Order getOrder(int minimumLength, int maximumLength, boolean includeUppercase,
-                                    boolean includeSpecial, boolean includeDigit) {
-        Faker faker = new Faker();
-        return Order.builder()
-                .id(faker.hashCode())
-                .petId(faker.hashCode())
-                .quantity(faker.hashCode())
-                .shipDate("1976-04-23T15:41:21.141+0000")
-                .status("placed")
-                .complete(true)
-                .build();
-    }
-
     public static Pet getPet(int minimumLength, int maximumLength, boolean includeUppercase,
                                 boolean includeSpecial, boolean includeDigit) {
         Faker faker = new Faker();
